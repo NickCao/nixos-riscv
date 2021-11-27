02195837b6311ea7331ae8e94606ce14c52e50c7
Nick Cao <nickcao@nichi.co>
2021-11-27 16:08:51 +0800

    use latest kernel package

--- flake.nix
+++ flake.nix
@@ -26,6 +26,7 @@
               generic-extlinux-compatible.enable = true;
             };
             boot.initrd.kernelModules = [ "nvme" "mmc_block" "mmc_spi" "spi_sifive" "spi_nor" ];
+            boot.kernelPackages = pkgs.linuxPackages_latest;
             boot.kernelPatches = map (patch: { name = patch; patch = "${pkgs.meta-sifive}/recipes-kernel/linux/files/${patch}"; }) [
               "0001-riscv-sifive-fu740-cpu-1-2-3-4-set-compatible-to-sif.patch"
               "0002-riscv-sifive-unmatched-update-regulators-values.patch"
