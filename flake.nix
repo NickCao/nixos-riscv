66839262245246fb0e5c9592fcacda2bd7d4ccfd
Nick Cao <nickcao@nichi.co>
2021-11-27 20:40:21 +0800

    uboot: drop upstreamed patch

--- flake.nix
+++ flake.nix
@@ -56,19 +56,6 @@
                     defconfig = "sifive_hifive_unmatched_fu740_defconfig";
                     extraPatches = map (patch: "${self.meta-sifive}/recipes-bsp/u-boot/files/riscv64/${patch}") [
                       "0001-riscv-sifive-unleashed-support-compressed-images.patch"
-                      "0002-drivers-clk-sifive-fu740-prci-replace-pciaux-with-pc.patch"
-                      "0003-board-sifive-unmatched-add-initial-support-for-a-pla.patch"
-                      "0004-riscv-sifive-fu740-kconfig-Enable-support-for-Openco.patch"
-                      "0005-riscv-sifive-fu740-Support-i2c-in-spl.patch"
-                      "0006-board-sifive-Add-an-interface-to-get-PCB-revision.patch"
-                      "0007-board-sifive-remove-the-command-for-setting-serial-n.patch"
-                      "0008-board-sifive-unmatched-refine-GEMGXL-initialized-fun.patch"
-                      "0009-board-sifive-unmatched-reset-USB-hub-PCIe-USB-bridge.patch"
-                      "0010-board-sifive-spl-Initialized-the-PWM-setting-in-the-.patch"
-                      "0011-board-sifive-Set-LED-s-color-to-purple-in-the-U-boot.patch"
-                      "0012-board-sifive-Set-LED-s-color-to-blue-before-jumping-.patch"
-                      "0013-board-sifive-spl-Set-remote-thermal-of-TMP451-to-85-.patch"
-                      "0014-riscv-sifive-Set-default-fdtfile-names.patch"
                       "0015-riscv-sifive-unmatched-leave-128MiB-for-ramdisk.patch"
                       "0016-riscv-sifive-unmatched-disable-FDT-and-initrd-reloca.patch"
                     ];
