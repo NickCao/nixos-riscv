aafe3f607bd284272af538429d8763310541522f
Nick Cao <nickcao@nichi.co>
2021-11-29 00:13:36 +0800

    add bootrom to hydra build products

--- flake.nix
+++ flake.nix
@@ -80,19 +80,21 @@
                     '';
                     filesToInstall = [ "u-boot.itb" "spl/u-boot-spl.bin" ];
                   };
-                  bootrom-unmatched = super.runCommand "bootrom.bin"
+                  bootrom-unmatched = super.runCommand "bootrom"
                     {
                       nativeBuildInputs = with super.buildPackages; [ gptfdisk ];
                     } ''
                     set +o pipefail
-                    tr '\0' '\377' < /dev/zero | dd of="$out" iflag=fullblock bs=1M count=32
+                    mkdir -p "$out/nix-support"
+                    tr '\0' '\377' < /dev/zero | dd of="$out/flash.bin" iflag=fullblock bs=1M count=32
                     sgdisk -g --clear -a 1 \
                       --new=1:40:2087     --change-name=1:spl   --typecode=1:5B193300-FC78-40CD-8002-E86C45580B47 \
                       --new=2:2088:10279  --change-name=2:uboot --typecode=2:2E54B353-1271-4842-806F-E436D6AF6985 \
                       --new=3:10280:10535 --change-name=3:env   --typecode=3:0FC63DAF-8483-4772-8E79-3D69D8477DE4 \
-                      "$out"
-                    dd if=${self.uboot-unmatched}/u-boot-spl.bin of="$out" bs=4096 seek=5 conv=sync
-                    dd if=${self.uboot-unmatched}/u-boot.itb  of="$out" bs=4096 seek=261 conv=sync
+                      "$out/flash.bin"
+                    dd if=${self.uboot-unmatched}/u-boot-spl.bin of="$out/flash.bin" bs=4096 seek=5 conv=sync
+                    dd if=${self.uboot-unmatched}/u-boot.itb  of="$out/flash.bin" bs=4096 seek=261 conv=sync
+                    echo "file bin \"$out/flash.bin\"" >> "$out/nix-support/hydra-build-products"
                   '';
                 })
               ];
