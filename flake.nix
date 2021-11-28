6efd04c0ca7197ecd02050515ffa614a72165c47
Nick Cao <nickcao@nichi.co>
2021-11-28 10:47:15 +0800

    enable ssh

--- flake.nix
+++ flake.nix
@@ -103,6 +103,8 @@
             services.udisks2.enable = false;
             security.polkit.enable = false;
             services.getty.autologinUser = "root";
+            services.openssh.enable = true;
+            users.users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLQwaWXeJipSuAB+lV202yJOtAgJSNzuldH7JAf2jji" ];
           })
         ];
       };
