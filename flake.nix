35354032f1d8301a57089737c49ddd679bd18887
Nick Cao <nickcao@nichi.co>
2021-11-30 14:17:41 +0800

    refactor into overlay

--- flake.nix
+++ flake.nix
@@ -8,6 +8,84 @@
       inherit (pkgs) qemu opensbi-unmatched uboot-unmatched bootrom-unmatched
         apacheHttpd emacs firefox imagemagick mysql nginx nodejs-17_x pandoc php postgresql subversion vim gtk3;
     };
+    overlay = final: prev: {
+      boost = prev.boost17x;
+      gtk3 = prev.gtk3.override { trackerSupport = false; };
+      git = prev.git.override { perlSupport = false; }; # https://github.com/NixOS/nixpkgs/issues/66741
+      xdg-utils = prev.coreutils; # also relies on perl
+      qemu = prev.qemu.override { gtkSupport = false; };
+      firefox-unwrapped = prev.firefox-unwrapped.override { ltoSupport = false; webrtcSupport = false; };
+      firefox = prev.wrapFirefox prev.firefox-unwrapped { };
+      meta-sifive = prev.fetchFromGitHub {
+        owner = "sifive";
+        repo = "meta-sifive";
+        rev = "2021.10.00";
+        sha256 = "sha256-TDlrAOOoK+3k/J1gDT1CkbxlfGfhSayZEzIjG1L3iPY=";
+      };
+      opensbi-unmatched = prev.stdenv.mkDerivation rec {
+        pname = "opensbi";
+        version = "0.9";
+        src = prev.fetchFromGitHub {
+          owner = "riscv";
+          repo = "opensbi";
+          rev = "v${version}";
+          sha256 = "sha256-W39R1RHsIM3yNwW/eukO+mPd9joPZLw+/XIJoH8agN8=";
+        };
+        patches = map (patch: "${final.meta-sifive}/recipes-bsp/opensbi/files/${patch}") [
+          "0001-Makefile-Don-t-specify-mabi-or-march.patch"
+        ];
+        hardeningDisable = [ "all" ];
+        makeFlags = [
+          "PLATFORM=generic"
+          "I=$(out)"
+        ];
+      };
+      uboot-unmatched = prev.buildUBoot rec {
+        version = "2022.01-rc2";
+        src = prev.fetchFromGitHub {
+          owner = "u-boot";
+          repo = "u-boot";
+          rev = "v${version}";
+          sha256 = "sha256-74jHYazqHguPnaYisr9qfafMukIU+5+jCoZ+jXvXEUg=";
+        };
+        defconfig = "sifive_unmatched_defconfig";
+        extraPatches = map (patch: "${final.meta-sifive}/recipes-bsp/u-boot/files/riscv64/${patch}") [
+          "0001-riscv-sifive-unleashed-support-compressed-images.patch"
+          "0015-riscv-sifive-unmatched-leave-128MiB-for-ramdisk.patch"
+          "0016-riscv-sifive-unmatched-disable-FDT-and-initrd-reloca.patch"
+        ] ++ [ ./u-boot-spi.patch ];
+        extraMakeFlags = [
+          "OPENSBI=${final.opensbi-unmatched}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin"
+        ];
+        extraConfig = ''
+          CONFIG_FS_EXT4=y
+          CONFIG_CMD_EXT4=y
+          CONFIG_SPL_SPI_FLASH_SUPPORT=y
+          CONFIG_SPL_DM_SPI_FLASH=y
+          CONFIG_SPL_SPI_LOAD=y
+          CONFIG_USE_ENV_SPI_BUS=y
+          CONFIG_ENV_SPI_BUS=1
+          CONFIG_SPI_FLASH_ISSI=y
+        '';
+        filesToInstall = [ "u-boot.itb" "spl/u-boot-spl.bin" ];
+      };
+      bootrom-unmatched = prev.runCommand "bootrom"
+        {
+          nativeBuildInputs = with prev.buildPackages; [ gptfdisk ];
+        } ''
+        set +o pipefail
+        mkdir -p "$out/nix-support"
+        tr '\0' '\377' < /dev/zero | dd of="$out/flash.bin" iflag=fullblock bs=1M count=32
+        sgdisk -g --clear -a 1 \
+          --new=1:40:2087     --change-name=1:spl   --typecode=1:5B193300-FC78-40CD-8002-E86C45580B47 \
+          --new=2:2088:10279  --change-name=2:uboot --typecode=2:2E54B353-1271-4842-806F-E436D6AF6985 \
+          --new=3:10280:10535 --change-name=3:env   --typecode=3:0FC63DAF-8483-4772-8E79-3D69D8477DE4 \
+          "$out/flash.bin"
+        dd if=${final.uboot-unmatched}/u-boot-spl.bin of="$out/flash.bin" bs=4096 seek=5 conv=sync
+        dd if=${final.uboot-unmatched}/u-boot.itb  of="$out/flash.bin" bs=4096 seek=261 conv=sync
+        echo "file bin \"$out/flash.bin\"" >> "$out/nix-support/hydra-build-products"
+      '';
+    };
     nixosConfigurations = {
       unmatched = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -15,95 +93,16 @@
           ({ config, pkgs, lib, modulesPath, ... }: {
             imports = [ "${modulesPath}/installer/sd-card/sd-image.nix" ];
             disabledModules = [ "profiles/all-hardware.nix" ];
+            nixpkgs = {
+              crossSystem.config = "riscv64-unknown-linux-gnu";
+              overlays = [ self.overlay ];
+            };
             sdImage = {
               populateRootCommands = ''
                 mkdir -p ./files/boot
                 ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
               '';
               populateFirmwareCommands = "";
-            };
-            nixpkgs = {
-              crossSystem.config = "riscv64-unknown-linux-gnu";
-              overlays = [
-                (self: super: {
-                  boost = super.boost17x;
-                  gtk3 = super.gtk3.override { trackerSupport = false; };
-                  git = super.git.override { perlSupport = false; }; # https://github.com/NixOS/nixpkgs/issues/66741
-                  xdg-utils = super.coreutils; # also relies on perl
-                  qemu = super.qemu.override { gtkSupport = false; };
-                  firefox-unwrapped = super.firefox-unwrapped.override { ltoSupport = false; webrtcSupport = false; };
-                  firefox = super.wrapFirefox self.firefox-unwrapped { };
-                  meta-sifive = super.fetchFromGitHub {
-                    owner = "sifive";
-                    repo = "meta-sifive";
-                    rev = "2021.10.00";
-                    sha256 = "sha256-TDlrAOOoK+3k/J1gDT1CkbxlfGfhSayZEzIjG1L3iPY=";
-                  };
-                  opensbi-unmatched = super.stdenv.mkDerivation rec {
-                    pname = "opensbi";
-                    version = "0.9";
-                    src = super.fetchFromGitHub {
-                      owner = "riscv";
-                      repo = "opensbi";
-                      rev = "v${version}";
-                      sha256 = "sha256-W39R1RHsIM3yNwW/eukO+mPd9joPZLw+/XIJoH8agN8=";
-                    };
-                    patches = map (patch: "${self.meta-sifive}/recipes-bsp/opensbi/files/${patch}") [
-                      "0001-Makefile-Don-t-specify-mabi-or-march.patch"
-                    ];
-                    hardeningDisable = [ "all" ];
-                    makeFlags = [
-                      "PLATFORM=generic"
-                      "I=$(out)"
-                    ];
-                  };
-                  uboot-unmatched = super.buildUBoot rec {
-                    version = "2022.01-rc2";
-                    src = super.fetchFromGitHub {
-                      owner = "u-boot";
-                      repo = "u-boot";
-                      rev = "v${version}";
-                      sha256 = "sha256-74jHYazqHguPnaYisr9qfafMukIU+5+jCoZ+jXvXEUg=";
-                    };
-                    defconfig = "sifive_unmatched_defconfig";
-                    extraPatches = map (patch: "${self.meta-sifive}/recipes-bsp/u-boot/files/riscv64/${patch}") [
-                      "0001-riscv-sifive-unleashed-support-compressed-images.patch"
-                      "0015-riscv-sifive-unmatched-leave-128MiB-for-ramdisk.patch"
-                      "0016-riscv-sifive-unmatched-disable-FDT-and-initrd-reloca.patch"
-                    ] ++ [ ./u-boot-spi.patch ];
-                    extraMakeFlags = [
-                      "OPENSBI=${self.opensbi-unmatched}/share/opensbi/lp64/generic/firmware/fw_dynamic.bin"
-                    ];
-                    extraConfig = ''
-                      CONFIG_FS_EXT4=y
-                      CONFIG_CMD_EXT4=y
-                      CONFIG_SPL_SPI_FLASH_SUPPORT=y
-                      CONFIG_SPL_DM_SPI_FLASH=y
-                      CONFIG_SPL_SPI_LOAD=y
-                      CONFIG_USE_ENV_SPI_BUS=y
-                      CONFIG_ENV_SPI_BUS=1
-                      CONFIG_SPI_FLASH_ISSI=y
-                    '';
-                    filesToInstall = [ "u-boot.itb" "spl/u-boot-spl.bin" ];
-                  };
-                  bootrom-unmatched = super.runCommand "bootrom"
-                    {
-                      nativeBuildInputs = with super.buildPackages; [ gptfdisk ];
-                    } ''
-                    set +o pipefail
-                    mkdir -p "$out/nix-support"
-                    tr '\0' '\377' < /dev/zero | dd of="$out/flash.bin" iflag=fullblock bs=1M count=32
-                    sgdisk -g --clear -a 1 \
-                      --new=1:40:2087     --change-name=1:spl   --typecode=1:5B193300-FC78-40CD-8002-E86C45580B47 \
-                      --new=2:2088:10279  --change-name=2:uboot --typecode=2:2E54B353-1271-4842-806F-E436D6AF6985 \
-                      --new=3:10280:10535 --change-name=3:env   --typecode=3:0FC63DAF-8483-4772-8E79-3D69D8477DE4 \
-                      "$out/flash.bin"
-                    dd if=${self.uboot-unmatched}/u-boot-spl.bin of="$out/flash.bin" bs=4096 seek=5 conv=sync
-                    dd if=${self.uboot-unmatched}/u-boot.itb  of="$out/flash.bin" bs=4096 seek=261 conv=sync
-                    echo "file bin \"$out/flash.bin\"" >> "$out/nix-support/hydra-build-products"
-                  '';
-                })
-              ];
             };
             boot.loader = {
               grub.enable = false;
