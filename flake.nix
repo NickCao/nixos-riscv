5149f8fc6847a87e297feb2e8938e7fd727f1d8b
Nick Cao <nickcao@nichi.co>
2021-12-10 16:48:32 +0800

    add a lot of go packages

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
       inherit (pkgs) qemu opensbi-unmatched uboot-unmatched bootrom-unmatched chromium
         apacheHttpd emacs firefox firefox-lto imagemagick mysql nginx nodejs-17_x pandoc php postgresql subversion vim gtk3;
       inherit (pkgs.libsForQt5) qtbase qtdeclarative qtmultimedia qtsvg qttools qtwebengine qtwebview;
-    } // pkgs.lib.filterAttrs (_: drv: pkgs.lib.isDerivation (builtins.tryEval drv).value) pkgs.gnome;
+    } // pkgs.lib.filterAttrs (_: drv: pkgs.lib.isDerivation (builtins.tryEval drv).value) pkgs.gnome
+    // import ./go-packages.nix { inherit pkgs; };
     overlay = final: prev: rec {
       libmysqlclient = null;
       boost = prev.boost17x;
