4eb7542b5a4c4939b8103ffad041477fb3e565f8
Nick Cao <nickcao@nichi.co>
2021-12-10 18:35:09 +0800

    try to run a full evaluation

--- flake.nix
+++ flake.nix
@@ -3,13 +3,17 @@
     nixpkgs.url = "github:NickCao/nixpkgs/riscv";
   };
   outputs = { self, nixpkgs }: {
-    hydraJobs = with self.nixosConfigurations.unmatched; {
-      unmatched = config.system.build.toplevel;
-      inherit (pkgs) qemu opensbi-unmatched uboot-unmatched bootrom-unmatched chromium
-        apacheHttpd emacs firefox firefox-lto imagemagick mysql nginx nodejs-17_x pandoc php postgresql subversion vim gtk3;
-      inherit (pkgs.libsForQt5) qtbase qtdeclarative qtmultimedia qtsvg qttools qtwebengine qtwebview;
-    } // pkgs.lib.filterAttrs (_: drv: pkgs.lib.isDerivation (builtins.tryEval drv).value) pkgs.gnome
-    // import ./go-packages.nix { inherit pkgs; };
+    hydraJobs = ((import "${nixpkgs}/pkgs/top-level/release-lib.nix" {
+      supportedSystems = [ "x86_64-linux" ];
+      nixpkgsArgs = {
+        config = { allowUnfree = false; inHydra = true; };
+        overlays = [ self.overlay ];
+      };
+    }).pkgsForCross
+      {
+        config = "riscv64-unknown-linux-gnu";
+      }
+      "x86_64-linux");
     overlay = final: prev: rec {
       libmysqlclient = null;
       boost = prev.boost17x;
