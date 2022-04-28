e54156bb3a734cb865e8a62a592c8a149966d542
Nick Cao <nickcao@nichi.co>
2022-04-28 16:42:29 +0800

    wip: add support for vision five

---
+++ unmatched.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, lib, modulesPath, ... }: {
+  sdImage = {
+    populateRootCommands = ''
+      mkdir -p ./files/boot
+      ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
+    '';
+    populateFirmwareCommands = "";
+  };
+  boot.loader = {
+    grub.enable = false;
+    generic-extlinux-compatible.enable = true;
+  };
+  boot.initrd.kernelModules = [ "nvme" "mmc_block" "mmc_spi" "spi_sifive" "spi_nor" "uas" "sdhci_pci" ];
+  boot.kernelParams = [ "console=ttySIF1" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPatches = map (patch: { name = patch; patch = "${pkgs.meta-sifive}/recipes-kernel/linux/files/${patch}"; }) [
+    "0001-riscv-sifive-fu740-cpu-1-2-3-4-set-compatible-to-sif.patch"
+    # "0002-riscv-sifive-unmatched-update-regulators-values.patch"
+    "0003-riscv-sifive-unmatched-define-PWM-LEDs.patch"
+    # "0004-riscv-sifive-unmatched-add-gpio-poweroff-node.patch"
+    # "0005-SiFive-HiFive-Unleashed-Add-PWM-LEDs-D1-D2-D3-D4.patch"
+    "0006-riscv-sifive-unleashed-define-opp-table-cpufreq.patch"
+    # "0007-riscv-enable-generic-PCI-resource-mapping.patch"
+    # "29868ae1478fe18231672da94c4e862a03218a25.patch"
+    # "riscv-sbi-srst-support.patch"
+    # "fa8b369129b0706d400e1dfe150c946e64f56df5.patch"
+  ] ++ [{
+    name = "sifive";
+    patch = null;
+    extraConfig = ''
+      SOC_SIFIVE y
+      PCIE_FU740 y
+      PWM_SIFIVE y
+      EDAC_SIFIVE y
+      SIFIVE_L2 y
+      RISCV_ERRATA_ALTERNATIVE y
+      ERRATA_SIFIVE y
+      ERRATA_SIFIVE_CIP_453 y
+      ERRATA_SIFIVE_CIP_1200 y
+    '';
+  }];
+  services.getty.autologinUser = "root";
+  services.openssh.enable = true;
+  environment.systemPackages = with pkgs; [
+    neofetch
+    mtdutils
+    lm_sensors
+    # waypipe
+    pciutils
+    glxinfo
+    radeontop
+    iperf3
+    gdb
+  ];
+  networking.wireless.iwd.enable = true;
+  hardware.firmware = with pkgs; [ firmwareLinuxNonfree ];
+  hardware.opengl.enable = true;
+  # programs.sway.enable = true;
+  users = {
+    mutableUsers = false;
+    users = {
+      root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLQwaWXeJipSuAB+lV202yJOtAgJSNzuldH7JAf2jji" ];
+    };
+  };
+}
---
+++ visionfive.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, lib, modulesPath, ... }: {
+  nixpkgs = {
+    crossSystem.config = "riscv64-unknown-linux-gnu";
+    config = {
+      allowUnfree = true;
+      allowBroken = true;
+    };
+  };
+  sdImage = {
+    populateRootCommands = ''
+      mkdir -p ./files/boot
+      ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
+    '';
+    populateFirmwareCommands = "";
+  };
+  boot.loader = {
+    grub.enable = false;
+    generic-extlinux-compatible.enable = true;
+  };
+  hardware.deviceTree.name = "starfive/jh7100-starfive-visionfive-v1.dtb";
+    boot.kernelParams = [
+    "console=tty0" "console=ttyS0,115200" "earlycon=sbi"
+    # https://github.com/starfive-tech/linux/issues/14
+    "stmmac.chain_mode=1"
+  ];
+  boot.initrd.kernelModules = [ "dw-axi-dmac-platform" "dw_mmc-pltfm" "spi-dw-mmio" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPatches = [{
+    name = "visionfive";
+    patch = null;
+    structuredExtraConfig = with pkgs.lib.kernel; {
+      SOC_STARFIVE = yes;
+      SERIAL_8250_DW = yes;
+      PINCTRL_STARFIVE = yes;
+      DW_AXI_DMAC_STARFIVE = yes;
+      PTP_1588_CLOCK = yes;
+      STMMAC_ETH = yes;
+      STMMAC_PCI = yes;
+    };
+  }];
+  services.getty.autologinUser = "root";
+  services.openssh.enable = true;
+  users = {
+    mutableUsers = false;
+    users = {
+      root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLQwaWXeJipSuAB+lV202yJOtAgJSNzuldH7JAf2jji" ];
+    };
+  };
+}
