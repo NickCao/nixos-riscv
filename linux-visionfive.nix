c5506046d69caba28158455fc37de0526ebdab62
Nick Cao <nickcao@nichi.co>
2022-04-29 20:02:01 +0800

    use kernel fork for baseline

---
+++ linux-visionfive.nix
@@ -0,0 +1,28 @@
+{ lib, stdenv, fetchFromGitHub, buildLinux, ... } @ args:
+
+buildLinux
+  (args // {
+    version = "5.16.11";
+
+    src = fetchFromGitHub {
+      owner = "starfive-tech";
+      repo = "linux";
+      rev = "d7f399d52baa99c715f257707040573768ba3d97";
+      sha256 = "sha256-Xi0sNUrLL7fCnDc5w+mxYJCwiPFaamdvJmKfvbP7ZbY=";
+    };
+
+    defconfig = "starfive_jh7100_fedora_defconfig";
+
+    structuredExtraConfig = with lib.kernel; {
+      SERIAL_8250_DW = yes;
+      PINCTRL_STARFIVE = yes;
+
+      # Doesn't build as a module
+      DW_AXI_DMAC_STARFIVE = yes;
+
+      # stmmac hangs when built as a module
+      PTP_1588_CLOCK = yes;
+      STMMAC_ETH = yes;
+      STMMAC_PCI = yes;
+    };
+  }) // (args.argsOverride or { })
--- visionfive.nix
+++ visionfive.nix
@@ -7,45 +7,37 @@
     };
   };
   sdImage = {
+    populateFirmwareCommands = "";
     populateRootCommands = ''
       mkdir -p ./files/boot
       ${config.boot.loader.generic-extlinux-compatible.populateCmd} -c ${config.system.build.toplevel} -d ./files/boot
     '';
-    populateFirmwareCommands = "";
   };
   boot.loader = {
     grub.enable = false;
     generic-extlinux-compatible.enable = true;
+    kernelPackages = pkgs.linuxPackagesFor (pkgs.callPackage ./linux-visionfive.nix {
+      kernelPatches = with pkgs.kernelPatches; [
+        bridge_stp_helper
+        request_key_helper
+      ];
+    });
+    kernelParams = [
+      "console=tty0"
+      "console=ttyS0,115200"
+      "earlycon=sbi"
+      # https://github.com/starfive-tech/linux/issues/14
+      "stmmac.chain_mode=1"
+    ];
+    initrd.kernelModules = [ "dw-axi-dmac-platform" "dw_mmc-pltfm" "spi-dw-mmio" ];
   };
   hardware.deviceTree.name = "starfive/jh7100-starfive-visionfive-v1.dtb";
-  boot.kernelParams = [
-    "console=tty0"
-    "console=ttyS0,115200"
-    "earlycon=sbi"
-    # https://github.com/starfive-tech/linux/issues/14
-    "stmmac.chain_mode=1"
-  ];
-  boot.initrd.kernelModules = [ "dw_mmc-pltfm" "spi-dw-mmio" ];
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelPatches = [{
-    name = "visionfive";
-    patch = null;
-    structuredExtraConfig = with pkgs.lib.kernel; {
-      SOC_STARFIVE = yes;
-      SERIAL_8250_DW = yes;
-      PINCTRL_STARFIVE = yes;
-      DW_AXI_DMAC_STARFIVE = yes;
-      PTP_1588_CLOCK = yes;
-      STMMAC_ETH = yes;
-      STMMAC_PCI = yes;
-    };
-  }];
+  systemd.services."serial-getty@hvc0".enable = false;
   services.getty.autologinUser = "root";
   services.openssh.enable = true;
   users = {
     mutableUsers = false;
-    users = {
-      root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLQwaWXeJipSuAB+lV202yJOtAgJSNzuldH7JAf2jji" ];
-    };
+    users.root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLQwaWXeJipSuAB+lV202yJOtAgJSNzuldH7JAf2jji" ];
   };
+  environment.systemPackages = with pkgs;[ neofetch ];
 }
