cc27dfa5697fc15375116898b31bf7b2522748d0
Nick Cao <nickcao@nichi.co>
2022-11-11 23:56:07 +0800

    unmatched: fix build

--- unmatched.nix
+++ unmatched.nix
@@ -10,12 +10,13 @@
     grub.enable = false;
     generic-extlinux-compatible.enable = true;
   };
+
+  boot.supportedFilesystems = lib.mkForce [ "btrfs" "vfat" "f2fs" "xfs" ];
   boot.initrd.kernelModules = [ "nvme" "mmc_block" "mmc_spi" "spi_sifive" "spi_nor" "uas" "sdhci_pci" ];
   boot.kernelParams = [ "console=ttySIF1" ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelPatches = map (patch: { name = patch; patch = "${pkgs.meta-sifive}/recipes-kernel/linux/files/${patch}"; }) [
     "0001-riscv-sifive-fu740-cpu-1-2-3-4-set-compatible-to-sif.patch"
-    "0003-riscv-sifive-unmatched-define-PWM-LEDs.patch"
     "0005-SiFive-HiFive-Unleashed-Add-PWM-LEDs-D1-D2-D3-D4.patch"
     "Revert-riscv-dts-sifive-unmatched-Link-the-tmp451-wi.patch"
   ] ++ [{
