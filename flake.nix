901b648fc3b56b80669566fafd98ec6f261982a9
Nick Cao <nickcao@nichi.co>
2023-01-09 19:48:44 +0800

    firmware-vf2: offer download

--- flake.nix
+++ flake.nix
@@ -62,11 +62,17 @@
         ];
         installPhase = ''
           runHook preInstall
+
           mkdir -p $out
           python3 ${starfive-tools}/spl_tool/create_sbl ${final.uboot-vf2}/u-boot-spl.bin $out/u-boot-spl.bin.normal.out
           substitute ${starfive-tools}/uboot_its/visionfive2-uboot-fit-image.its visionfive2-uboot-fit-image.its \
             --replace fw_payload.bin ${final.opensbi-vf2}/share/opensbi/lp64/generic/firmware/fw_payload.bin
           mkimage -f visionfive2-uboot-fit-image.its -A riscv -O u-boot -T firmware $out/visionfive2_fw_payload.img
+
+          mkdir -p "$out/nix-support"
+          echo "file bin \"$out/u-boot-spl.bin.normal.out\"" >> "$out/nix-support/hydra-build-products"
+          echo "file bin \"$out/visionfive2_fw_payload.img\"" >> "$out/nix-support/hydra-build-products"
+
           runHook postInstall
         '';
       };
