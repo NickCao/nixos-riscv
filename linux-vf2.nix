04c67d3098314578ae25c1c4e256e9022e6b22c4
Nick Cao <nickcao@nichi.co>
2023-05-15 15:40:18 +0800

    linux-vf2: 6.3.0 -> 6.4.0-rc2

--- linux-vf2.nix
+++ linux-vf2.nix
@@ -5,7 +5,7 @@
 } @ args:
 
 let
-  modDirVersion = "6.3.0";
+  modDirVersion = "6.4.0-rc2";
 in
 buildLinux (args // {
   inherit modDirVersion;
@@ -14,8 +14,8 @@ buildLinux (args // {
   src = fetchFromGitHub {
     owner = "NickCao";
     repo = "linux";
-    rev = "fc80774f1a7b4c8432952b20da8c30a8ab7f0ac2";
-    sha256 = "sha256-f4euRON+QHNllQnU0cxl9ynOKMobys51g1ZeSKKkSv0=";
+    rev = "f4be6cc5d56d372f05ddc1b2a0745f8bd2af9bbf";
+    sha256 = "sha256-tmgvyE92YdnH2G1sE61ZInKPuDogdSjp8KRy6OHYatQ=";
   };
 
   structuredExtraConfig = with lib.kernel; {
