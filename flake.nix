f350f2eac9e066628533cbc15492e8811447a6ee
Nick Cao <nickcao@nichi.co>
2023-09-14 18:11:20 -0400

    add thunderbird to hydra jobs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,12 @@
     hydraJobs = with self.nixosConfigurations.qemu;
       let vf2 = pkgs.callPackage "${nixos-hardware}/starfive/visionfive/v2/firmware.nix" { }; in {
         visionfive2 = self.nixosConfigurations.visionfive2.config.system.build.sdImage;
-        inherit (pkgs) qemu opensbi firefox-unwrapped;
+        inherit (pkgs)
+          qemu
+          opensbi
+          firefox-unwrapped
+          thunderbird-unwrapped
+          ;
         spl-vf2 = vf2.spl;
         uboot-fit-image-vf2 = vf2.uboot-fit-image;
         edk2-vf2 = pkgs.pkgsCross.riscv64-embedded.callPackage ./edk2-vf2.nix { };
