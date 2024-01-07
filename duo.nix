b57c9b31db9fd59bb439bfd5e62865c1085361c2
Nick Cao <nickcao@nichi.co>
2024-01-07 18:58:21 -0500

    no longer need to populate init

--- duo.nix
+++ duo.nix
@@ -191,10 +191,6 @@ in
       cp ${./prebuilt/fip.bin}         firmware/fip.bin
       cp ${config.system.build.bootsd} firmware/boot.sd
     '';
-    populateRootCommands = ''
-      mkdir -p files/sbin
-      ln -s ${config.system.build.toplevel}/init files/sbin/init
-    '';
   };
 
 }
