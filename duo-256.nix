125d0402210072adb8bc9fd78d8f72b889572027
Chris McDonough <chrism@plope.com>
2024-02-21 15:50:43 -0500

    put docs for 256M into README.md

--- duo-256.nix
+++ duo-256.nix
@@ -1,7 +1,5 @@
 { config, lib, pkgs, modulesPath, ... }:
 
-# Native ethernet is untested. GPIO is untested.  RNDIS works.
-
 # The cv1812cp_milkv_duo256m_sd.dtb and fip-duo256.bin (aka fip.bin) files in
 # the prebuilt/ dir used by this module were generated on Ubuntu via "./build.sh
 # lunch" within a fork of Milk V's duo-buildroot-sdk repo at
@@ -37,78 +35,6 @@
 
 # will let you drop into a prompt to find it in /mnt-root/nix/store
 
-# You will be able to ssh to the Duo after configuring a an Ethernet connection
-# that uses the RNDIS interface enabled by plugging the Duo in to your host
-# system.  Unlike the Milk V vendor buildroot image, DHCP is not used by the
-# NixOS image to manage the addresses of the Duo or the host. Instead, you will
-# need to configure the host to use a static IP address.
-#
-# When you plug the Duo in via USB C, an "ifconfig" of the host it's connected
-# will reveal a new Ethernet interface on your host machine something like
-# "enp0s20f0u7u2".  This is the interface that must be configured in order to
-# connect to the Duo. Under KDE, I used the Network Settings "Connections" pane
-# to create a new "Wired Ethernet" connection with the following settings:
-
-# "Wired"
-#   Restrict to device: enp0s20f0u7u2 (00:22:82:FF:FF:20)
-#
-# IPv4:
-#   Method: Manual
-#   Address/Netmask/Gateway: 192.168.58.1/255.255.255.0/0.0.0.0
-
-# You will likely also need to restrict any existing Ethernet interfaces in
-# their "Wired" tabs to the real Ethernet interface on your host machine to
-# prevent the system from trying to use the RNDIS interface to obtain its normal
-# DHCP settings. For me, I had to add "Restrict to Device: enp1s0
-# (A4:BB:6D:9B:37:2D)" in my primary wired Ethernet connection settings.
-#
-# After applying those settings, you should be able to connect to the Duo via
-# "ssh root@192.168.58.2".  The password is "milkv".
-#
-# NB: it takes about 30 seconds after the Duo boots for the ssh server to start
-# after the interface has been recognized by the host, be patient.
-#
-# You can give the Duo access to the larger internet by setting up
-# NAT/masquerading on the host.  You can do the following on the host the Duo is
-# connected to set up NAT.
-#
-#   "echo 1 > /proc/sys/net/ipv4/ip_forward"
-#
-# or (in NixOS) via declarative sysctl setup
-#
-#   boot.kernel.sysctl = { "net.ipv4.conf.all.forwarding" = true; };
-
-# Then execute a variant of the following nftables script (I was unable to
-# quickly make this work declaratively in my NixOS host config via
-# "networking.nftables.ruleset"; it's probably possible) which enables the host
-# to route packets on behalf of the Duo via NAT/masquerade to and from the
-# internet.  Change the interface names as necessary.  Once executed, the Duo
-# will be able to communicate with the outside world, using the host as a
-# router. NB: on a NixOS host machine, you do *not* need
-# "networking.firewall.enable = true;" for this to work but
-# "networking.nftables.enable = true;" makes the nft command available.
-
-#    #!/run/current-system/sw/bin/nft -f
-#
-#    # enp1s0 is my ethernet interface, connected to my Internet router.
-#    # enp0s20f0u7u2 is the RNDIS interface created by attaching the Duo to
-#    # the host via USB.
-#
-#    table ip duo_table { chain duo_nat { type nat hook postrouting priority 0;
-#           policy accept; oifname "enp1s0" masquerade
-#           }
-#
-#          chain duo_forward {
-#                   type filter hook forward priority 0; policy accept;
-#                   iifname "enp0s20f0u7u2" oifname "enp1s0" accept
-#           }
-#    }
-#
-#
-# NB: In order for the Duo to connect to the internet, by default, without
-# changes to this Nix file, the host must be contactable via the IP address
-# "192.168.58.1" because this Nix file hardcodes that IP address as the Duo's
-# default gateway.
 
 let
   duo-buildroot-sdk = pkgs.fetchFromGitHub {
