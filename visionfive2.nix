1a7c11e49a0d2711f844f79a24083a5eed6d2786
Nick Cao <nickcao@nichi.co>
2024-06-02 15:47:59 -0400

    visionfive2: disable docker

--- visionfive2.nix
+++ visionfive2.nix
@@ -5,8 +5,6 @@
     crossSystem.config = "riscv64-unknown-linux-gnu";
   };
 
-  virtualisation.docker.enable = true;
-
   programs.less.lessopen = null;
 
   environment.systemPackages = with pkgs;[ neofetch iperf3 ];
