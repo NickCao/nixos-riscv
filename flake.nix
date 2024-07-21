391499e0ebbacdf8519db4f702c293ed2b1e484b
Nick Cao <nickcao@nichi.co>
2024-07-21 11:20:27 -0400

    Add thunderbird 128 to hydra jobs

--- flake.nix
+++ flake.nix
@@ -3,62 +3,55 @@
     nixpkgs.url = "github:NickCao/nixpkgs/riscv";
     nixos-hardware.url = "github:NickCao/nixos-hardware";
   };
-  outputs = { self, nixpkgs, nixos-hardware }: {
-    hydraJobs = with self.nixosConfigurations.qemu;{
-      visionfive2 = self.nixosConfigurations.visionfive2.config.system.build.sdImage;
-      duo = self.nixosConfigurations.duo.config.system.build.sdImage;
-      duo-256 = self.nixosConfigurations.duo-256.config.system.build.sdImage;
-      duos = self.nixosConfigurations.duos.config.system.build.sdImage;
-      inherit (pkgs)
-        qemu
-        opensbi
-        firefox-unwrapped
-        firefox-esr-unwrapped
-        thunderbird-unwrapped
-        postgresql_12
-        postgresql_13
-        postgresql_14
-        postgresql_15
-        postgresql_16
-        nodejs_18
-        nodejs_20
-        nodejs_22
-        ;
-      qt5 = {
-        inherit (pkgs.qt5)
-          qtbase
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-hardware,
+    }:
+    {
+      hydraJobs = with self.nixosConfigurations.qemu; {
+        visionfive2 = self.nixosConfigurations.visionfive2.config.system.build.sdImage;
+        duo = self.nixosConfigurations.duo.config.system.build.sdImage;
+        duo-256 = self.nixosConfigurations.duo-256.config.system.build.sdImage;
+        duos = self.nixosConfigurations.duos.config.system.build.sdImage;
+        inherit (pkgs)
+          qemu
+          opensbi
+          firefox-unwrapped
+          firefox-esr-unwrapped
+          postgresql_12
+          postgresql_13
+          postgresql_14
+          postgresql_15
+          postgresql_16
+          nodejs_18
+          nodejs_20
+          nodejs_22
           ;
+        thunderbird = pkgs.thunderbird.unwrapped;
+        thunderbird-128 = pkgs.thunderbird-128.unwrapped;
+        qt5 = {
+          inherit (pkgs.qt5) qtbase;
+        };
+        edk2-vf2 = pkgs.pkgsCross.riscv64-embedded.callPackage ./edk2-vf2.nix { };
       };
-      edk2-vf2 = pkgs.pkgsCross.riscv64-embedded.callPackage ./edk2-vf2.nix { };
-    };
-    nixosConfigurations = {
-      qemu = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./common.nix
-          ./qemu.nix
-        ];
-      };
-      duo = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./duo.nix
-        ];
-      };
-      duo-256 = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./duo-256.nix
-        ];
-      };
-      duos = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./duos.nix
-        ];
-      };
-      visionfive2 = nixpkgs.lib.nixosSystem {
-        modules = [
-          "${nixos-hardware}/starfive/visionfive/v2/sd-image-installer.nix"
-          ./visionfive2.nix
-        ];
+      nixosConfigurations = {
+        qemu = nixpkgs.lib.nixosSystem {
+          modules = [
+            ./common.nix
+            ./qemu.nix
+          ];
+        };
+        duo = nixpkgs.lib.nixosSystem { modules = [ ./duo.nix ]; };
+        duo-256 = nixpkgs.lib.nixosSystem { modules = [ ./duo-256.nix ]; };
+        duos = nixpkgs.lib.nixosSystem { modules = [ ./duos.nix ]; };
+        visionfive2 = nixpkgs.lib.nixosSystem {
+          modules = [
+            "${nixos-hardware}/starfive/visionfive/v2/sd-image-installer.nix"
+            ./visionfive2.nix
+          ];
+        };
       };
     };
-  };
 }
